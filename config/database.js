72c20bc8220dec74b6cc639d3621ffdac8a22820
devinnow8 <devinnow8@gmail.com>
2023-06-09 17:54:03 +0530

    minor change

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
-    // strapi-api/config/database.js
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapidb'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '1234'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          // ssl: {
-          //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          // },
-        },
-        debug: false,
-      },
-    });
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgress",
+    connection: {
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapidb"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "1234"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      // },
+    },
+    debug: false,
+  },
+});
 
 // const path = require('path');
 
