09d3f42c19eea0696ee517aaa96443fe9f6032ac
devinnow8 <devinnow8@gmail.com>
2023-06-13 11:25:32 +0530

    login sign-up api

--- src/api/sign-up/controllers/sign-up.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sign-up controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sign-up.sign-up');
--- src/api/sign-up/services/sign-up.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sign-up service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::sign-up.sign-up');
