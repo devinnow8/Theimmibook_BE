f02d54cb5a756c3908b912b982cc747c2c128d19
devinnow8 <devinnow8@gmail.com>
2023-06-15 17:49:10 +0530

    crud api of job

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // ...
+    'seo': {
+      enabled: true,
+      resolve: './src/plugins/seo'
+    },
+    // ...
+    // 'seo-backup':{
+    //     enabled:false,
+    //     resolve:'.src/plugins/seo-backup'
+    // },
+  }
--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '192.168.1.36'),
   // debug:true,
   port: env.int('PORT', 1337),
   app: {
