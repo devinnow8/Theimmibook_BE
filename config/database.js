b69e6937837c8cb194624d86cf039ff5a5aa5424
devinnow8 <devinnow8@gmail.com>
2023-06-26 07:18:19 +0530

    minor change

--- config/database.js
+++ config/database.js
@@ -9,9 +9,9 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", "db_user"),
       password: env("DATABASE_PASSWORD", "password"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
-      // ssl: {
-      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      // },
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
