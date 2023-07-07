0d58e53a4ce987fd73fe13b428b4943c6ac50833
devinnow8 <ryadav@innow8apps.com>
2023-07-07 12:39:45 +0530

    IMAGE

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,8 +7,20 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
           upgradeInsecureRequests: null,
         },
       },
@@ -18,18 +30,19 @@ module.exports = [
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
-      formidable: {
-        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
-    },
-  },
+  "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+//   {
+//     name: "strapi::body",
+//     config: {
+//       formLimit: "256mb", // modify form body
+//       jsonLimit: "256mb", // modify JSON body
+//       textLimit: "256mb", // modify text body
+//       formidable: {
+//         maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+//       },
+//     },
+//   },
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -26,30 +26,30 @@
 //     },
 
 //   }
-  module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-    // ...
-  });
-  
-module.exports = {
+module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
-    }
-  }
-};
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  // ...
+});
+
+// module.exports = {
+//   // ...
+//   upload: {
+//     config: {
+//       sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
+//     }
+//   }
+// };
